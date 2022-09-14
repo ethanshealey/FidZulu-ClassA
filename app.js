7d400aea9f7a3132beca7f88a5cba824528c781f
= <ethan.shealey@gmail.com>
2022-09-14 10:55:05 -0400

    minor update

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bikesRouter = require('./routes/bikes');
+var foodRouter = require('./routes/food');
+var toysRoutes = require('./routes/toys');
 
 var app = express();
 
@@ -20,7 +22,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/classA/bikes', bikesRouter);
+app.use('/classA/toys', toysRoutes);
+app.use('/classA/food', foodRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
